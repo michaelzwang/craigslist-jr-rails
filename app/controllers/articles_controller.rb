399343a939a94d6639acdfcc30fed30a3366b1e8
Jack Hamilton & Michael Wang <pair+jacklh9.michaelzwang@devbootcamp.com>
2016-04-11 12:34:21 -0700

    Add basic craigslist article creation, editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,29 @@
 class ArticlesController < ApplicationController
 
+  def create
+    @article = Article.new(article_params)
+    @article.generate_hash
+    @article.save
+    redirect_to edit_article_path(@article, key: @article.hash_key)
+  end
+
+  def new
+    @article = Article.new
+    @categories = Category.all
+  end
+
+  def edit
+    @article = Article.find_by(id: params[:id], hash_key: params[:key])
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+ private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :contact, :price, :category_id, :location)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
+  validates :title, :contact, :price, :category_id, presence: true
   belongs_to :category
+
+  def generate_hash
+    self.hash_key = SecureRandom.hex(n=3)
+  end
+
+
+
 end
