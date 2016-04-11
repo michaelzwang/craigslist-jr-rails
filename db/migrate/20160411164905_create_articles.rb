674f5c78ac47236783a360e524634231e4c39d5e
Jack Hamilton & Michael Wang <pair+jacklh9.michaelzwang@devbootcamp.com>
2016-04-11 10:55:09 -0700

    render categories

---
+++ db/migrate/20160411164905_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :body
+      t.string :contact, null: false
+      t.decimal :price, :count, 0.00
+      t.integer :category_id
+      t.string :location
+      t.string :hash_key, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
